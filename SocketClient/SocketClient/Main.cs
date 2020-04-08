7215e89d521cfd891933cf3f7fb50f705bb2328b
pengxiaokang <pengxiaokang@auxgroup.com>
2020-04-08 10:35:27 +0800

    first commit

---
+++ SocketClient/SocketClient/Main.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SocketClient
+{
+    public partial class Main : Form
+    {
+        //创建一个客户端套接字和一个负责监听服务端请求的线程
+        Thread threadClient = null;
+        Socket socketClient = null;
+
+        public Main()
+        {
+            InitializeComponent();
+            StartPosition = FormStartPosition.CenterScreen;
+            //关闭对文本框的非法线程操作检查
+            TextBox.CheckForIllegalCrossThreadCalls = false;
+            this.button1.Enabled = false;
+            this.button1.Visible = false;
+            this.textBox2.Visible = false;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //调用ClientSendMessage方法，将文本框中输入的信息发送到服务端
+            ClientSendMsg(this.textBox2.Text.Trim());
+            this.textBox2.Clear();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.button2.Enabled = false;
+            //定义一个套接字监听
+            socketClient = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            //获取文本框中的IP地址
+            IPAddress address = IPAddress.Parse("192.168.1.112");//192.168.1.112
+            //将获取的Ip地址和端口绑定在网络节点上
+            IPEndPoint point = new IPEndPoint(address, 12200);
+
+            try
+            {
+                //客户端套接字连接到网络节点上，用的是Connect
+                socketClient.Connect(point);
+                this.button1.Enabled = true;
+                this.button1.Visible = true;
+                this.textBox2.Visible = true;
+                this.textBox1.AppendText("连接成功！IP:" + address.ToString() + "\r\n");
+
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine("连接失败\r\n" + "失败原因：" + ex.Message);
+                this.textBox1.AppendText("连接失败\r\n" + "失败原因：" + ex.Message);
+
+                return;
+            }
+            threadClient = new Thread(recv);
+            threadClient.IsBackground = true;
+            threadClient.Start();
+
+        }
+        //接收服务端发来的消息
+        void recv()
+        {
+            int x = 0;
+            //持续监听服务端发来的消息
+            while (true)
+            {
+                try
+                {
+                    //定义一个1M的内存缓冲区，用于临时存储接收到的消息
+                    byte[] arrRecvmsg = new byte[1024 * 1024 * 500];
+                    //将客户端套接字接收到的数据存入到内存缓冲区中，并获取长度
+                    int length = socketClient.Receive(arrRecvmsg);
+                    //将套接字获取到的字符数组转换为可以看懂的字符串
+                    string strRevMsg = Encoding.UTF8.GetString(arrRecvmsg, 0, length);
+                    this.textBox1.AppendText("获取信息为：" + strRevMsg + "\r\n");
+                    if (x == 1)
+                    {
+                        this.textBox1.AppendText("服务器：" + DateTime.Now.ToString() + "\r\n" + strRevMsg + "\r\n\n");
+                        Debug.WriteLine("服务器：" + DateTime.Now.ToString() + "\r\n" + strRevMsg + "\r\n\n");
+                    }
+                    else
+                    {
+                        this.textBox1.AppendText( strRevMsg + "\r\n");
+                        Debug.WriteLine(strRevMsg + "\r\n");
+                        x = 1;
+                    }
+
+                }
+                catch (Exception ex)
+                {
+                    this.textBox1.AppendText("远程服务器已经中断连接！" + "\r\n\n" + "原因：" + ex.Message + "\r\n\n");
+                    Debug.WriteLine("远程服务器已经中断连接！" + "\r\n\n" + "原因：" + ex.Message + "\r\n\n");
+                    break;
+                }
+            }
+        }
+        //发送字符信息到服务端
+        void ClientSendMsg(string sendMsg)
+        {
+            try
+            {
+                //将输入的内容字符串转换为机器可以识别的字节数组
+                byte[] arrClientSendMsg = Encoding.UTF8.GetBytes(sendMsg);
+                //调用客户端套接字发送字节数组
+                socketClient.Send(arrClientSendMsg);
+                //将发送的信息追加到聊天内容文本框中
+                Debug.WriteLine("Hello……" + ":" + DateTime.Now.ToString() + "\r\n" + sendMsg + "\r\n\n");
+                this.textBox1.AppendText("Hello……" + ":" + DateTime.Now.ToString() + "\r\n" + sendMsg + "\r\n\n");
+
+
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine("远程服务器已经中断连接" + "\r\n\n");
+                Debug.WriteLine("远程服务器已经中断连接" + "\r\n\n");
+            }
+        }
+    }
+}
---
+++ SocketService/SocketService/SocketServer.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SocketService
+{
+    class SocketServer
+    {
+        //和客户端通信的套接字
+        static Socket client_socket = null;
+        //集合：存储客户端信息
+        static Dictionary<string, Socket> clientConnectionItems = new Dictionary<string, Socket> { };
+        public static void Main(string[] args)
+        {
+            //和客户端通信的套接字，监听客户端发来的消息，三个参数：IP4寻找协议，流失连接，TCP协议
+            client_socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+            //服务端发送信息需要一个IP地址和端口号
+            IPAddress address = IPAddress.Parse("127.0.0.1");//传入本机IP地址
+            //将IP地址和端口号绑定到网络节点point上
+            IPEndPoint point = new IPEndPoint(address, 5000);//5000端口专门用来监听，5000为本机未占用的可用端口，可自定
+            //监听绑定的网络节点
+            client_socket.Bind(point);
+
+            //将套接字的监听队列长度限制为20
+            client_socket.Listen(20);
+
+            //负责监听客户端的线程
+            Thread client_thread = new Thread(watchconnecting);
+            //将窗体线程设置为与后台同步，跟随主线程结束而结束
+            client_thread.IsBackground = true;
+            //启动线程
+            client_thread.Start();
+            Console.WriteLine("开启监听……");
+            Console.WriteLine("点击输入任意数据回车退出程序……");
+            Console.ReadKey();
+            Console.WriteLine("退出监听，并关闭程序");
+
+        }
+        //监听客户端发来的请求
+        static void watchconnecting()
+        {
+            Socket connection = null;
+            //持续不断监听客户端发来的请求
+            while (true)
+            {
+                try
+                {
+                    connection = client_socket.Accept();
+                }
+                catch (Exception ex)
+                {
+                    //套接字监听异常
+                    Console.WriteLine(ex.Message);
+                    break;
+                }
+                //获取客户端的Ip和端口号
+                IPAddress clientIp = (connection.RemoteEndPoint as IPEndPoint).Address;
+                int clientPort = (connection.RemoteEndPoint as IPEndPoint).Port;
+
+                //让客户显示“连接成功”的信息
+                string sendMsg = "连接服务端成功！\r\n" + "本地IP：" + clientIp + ",本地端口：" + clientPort.ToString();
+                byte[] arrSendMsg = Encoding.UTF8.GetBytes(sendMsg);
+                connection.Send(arrSendMsg);
+
+                //客户端网络结点号
+                string remoteEndPoint = connection.RemoteEndPoint.ToString();
+                //显示与客户端连接情况:
+                Console.WriteLine("成功与" + remoteEndPoint + "客户端建立连接！\r\n");
+                //添加客户端信息
+                clientConnectionItems.Add(remoteEndPoint, connection);
+                IPEndPoint netpoint = connection.RemoteEndPoint as IPEndPoint;
+                //创建一个线程通信
+                ParameterizedThreadStart pts = new ParameterizedThreadStart(recv);
+                Thread thread = new Thread(pts);
+                //设置后台进程，跟随主线程退出而退出
+                thread.IsBackground = true;
+                //启动线程
+                thread.Start(connection);
+
+            }
+        }
+
+        //接收客户端发来的消息，客户端套接字对象
+        static void recv(object socketclientpara)
+        {
+            Socket socketServer = socketclientpara as Socket;
+            while (true)
+            {
+                //创建一个内存缓存区，其大小为1024*1024字节，即1M
+                byte[] arrServiceRecMsg = new byte[1024 * 1024];
+                try
+                {
+                    int length = socketServer.Receive(arrServiceRecMsg);
+                    //将机器接收的字节数组转换为字符串
+                    string strRecMsg = Encoding.UTF8.GetString(arrServiceRecMsg, 0, length);
+                    //将发送的字符串信息附加到文本框txtMsg上
+                    Console.WriteLine("客户端：" +socketServer.RemoteEndPoint + ",time:" + DateTime.Now.ToString() + "\r\n" + strRecMsg + "\r\n\n");
+                    //socketServer.Send(Encoding.UTF8.GetBytes("测试socketServer是否可以发送数据给client"));
+                    socketServer.Send(Encoding.UTF8.GetBytes("通过server可以发送数据给client"));
+
+                }
+                catch (Exception ex)
+                {
+                    clientConnectionItems.Remove(socketServer.RemoteEndPoint.ToString());
+                    Console.WriteLine("Client Count:" + clientConnectionItems.Count);
+
+                    //提示套接字监听异常
+                    Console.WriteLine("客户端" + socketServer.RemoteEndPoint + "已经中断连接" + "\r\n" + ex.Message + "\r\n" + ex.StackTrace +"\r\n");
+                    //关闭之前accept出来的和客户端进行通讯的套接字
+                    socketServer.Close();
+                    break;
+                }
+            }
+        }
+    }
+}
